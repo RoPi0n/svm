f7ffbf738d0fa543b6161eb2da2b48aab02ea751
pavel151 <pavel151@inbox.ru>
2018-02-12 23:17:32 +0700

    Add files via upload

--- test/summ.asm
+++ test/summ.asm
@@ -1,59 +1,45 @@
-import
-  from "crt.lib"
-   PrintLn "PRINTLN"
-   InputLn "INPUTLN"
-  end from
-  from "bf.lib"
-   StrToFloat "STRTOFLOAT"
-  end from
-end import
+import PrintLn "crt.lib" "PRINTLN"
+import InputLn "crt.lib" "INPUTLN"
+import StrToFloat "bf.lib" "STRTOFLOAT"
 
 
-const
-  str s_0 "Enter two digits:"
-  str s_1 "Summ:"
-  int MemSz 2
-end const
+str s_0 "Enter two digits:"
+str s_1 "Summ:"
 
+var a,b
 
-code
-  EntryPoint:
-    pushc MemSz
-	gpm
-	memsz
-	gc
-	pushc s_0
-	gpm
-	pushc PrintLn
-	gpm
-	invoke
-	pushc InputLn
-	gpm
-	invoke
-	pushc StrToFloat
-	gpm
-	invoke
-	peek 0
-	pop
-	pushc InputLn
-	gpm
-	invoke
-	pushc StrToFloat
-	gpm
-	invoke
-	peek 1
-	gc
-	pop
-	push 0
-	
-	push 1
-	add
-	pushc s_1
-	gpm
-	pushc PrintLn
-	gpm
-	invoke
-	pushc PrintLn
-	gpm
-	invoke
-end code
+Main:
+  push !s_0
+  gpm
+  push !PrintLn
+  gpm
+  invoke
+  push !InputLn
+  gpm
+  invoke
+  push !StrToFloat
+  gpm
+  invoke
+  peek $a
+  pop
+  push !InputLn
+  gpm
+  invoke
+  push !StrToFloat
+  gpm
+  invoke
+  peek $b
+  gc
+  pop
+  push $a
+  push $b
+  add
+  push !s_1
+  gpm
+  push !PrintLn
+  gpm
+  invoke
+  pushc PrintLn
+  gpm
+  invoke
+  gc
