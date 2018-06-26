98850ca8b1420048a5abdfde6cf56be38aec27f0
Pavel Shiryaev <pavel151@inbox.ru>
2018-06-27 02:05:27 +0700

    Add files via upload

---
+++ ide/backup/aboutform.pas
@@ -0,0 +1,44 @@
+unit aboutform;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Image1: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Close;
+end;
+
+end.
+
---
+++ ide/backup/global.pas
@@ -0,0 +1,13 @@
+unit global;
+
+{$mode objfpc}{$H+}
+
+interface
+
+type
+  TOpenOp = (opNew, opOpen);
+
+implementation
+
+end.
+
