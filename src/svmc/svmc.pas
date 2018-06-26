2fdf1d19aab5a125ba8638adb0260d72c3dbb318
Pavel Shiryaev <pavel151@inbox.ru>
2018-06-27 01:55:02 +0700

    Delete svmc.pas

--- src/svmc/svmc.pas
+++
@@ -1,9 +0,0 @@
-program svmc;
-{$AppType Console}
-
-procedure StartVM; stdcall; {$IfDef Windows}external 'svm_l.dll'{$EndIf}
-  {$IfDef UNIX}external 'svm_l.so'{$EndIf} name '_SVML_RUN';
-
-begin
-  StartVM;
-end.
